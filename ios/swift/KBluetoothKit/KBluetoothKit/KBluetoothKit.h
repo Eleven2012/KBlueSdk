b519c648489298b46b39af3792fe2b02e698c209
yulu.kong <yulu.kong@ubtrobot.com>
2020-04-07 14:31:14 +0800

    init project

---
+++ ios/swift/KBluetoothKit/KBluetoothKit/KBluetoothKit.h
@@ -0,0 +1,19 @@
+//
+//  KBluetoothKit.h
+//  KBluetoothKit
+//
+//  Created by yulu kong on 2020/4/7.
+//  Copyright © 2020 yulu kong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KBluetoothKit.
+FOUNDATION_EXPORT double KBluetoothKitVersionNumber;
+
+//! Project version string for KBluetoothKit.
+FOUNDATION_EXPORT const unsigned char KBluetoothKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KBluetoothKit/PublicHeader.h>
+
+
